613278c8e8df1770336efc48913df03d7a5cc337
Antonette Caldwell <aacaldwell42@outlook.com>
2021-06-04 17:46:00 -0500

    Printed ascii 'A'

--- boot.s
+++ boot.s
@@ -2,9 +2,13 @@
     .global _start
 
     _start:
-        jmp _start
-        ljmpw $0xFFFFFFF0, $0
+        mov $0x0e41, %ax
+        int $0x10
 
     .org 510
     .byte 0x55
     .byte 0xaa
+
+    .hang:
+        hlt
+        jmp .hang
